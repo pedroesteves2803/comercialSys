ff06cfa66e6d859f0caedf52779155401a3ebbad
PEDRO DE ASSUNCAO ESTEVES <pedro.aesteves1@senacsp.edu.br>
2021-04-28 14:06:11 -0300

    Adicionar arquivos de projeto.

---
+++ comeercialSys/Classes/Pedido.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace comeercialSys.Classes
+{
+
+    public class Pedido
+    {
+        public int Id {get; set;}
+        public DateTime Data { get; set;}
+
+        public Cliente Cliente { get; set; }
+
+        public Usuario Usuario { get; set; }
+
+        public double Desconto { get; set; }
+
+        public string situacao { get; set; }
+
+        public List<Item> Items { get; set; }
+    }
+}
